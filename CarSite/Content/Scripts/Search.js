2c53e284e169baea13e083792fc0d8fb129592c9
vien <thuvienpmp@gmail.com>
2016-02-18 09:49:57 +0700

    repair menu left

--- CarSite/Content/Scripts/Search.js
+++ CarSite/Content/Scripts/Search.js
@@ -1,4 +1,9 @@
-﻿
+﻿$("#search-car-firm").hide();
+
+$("#seared").click(function () {
+    $("#search-car-firm").toggle();
+});
+
 $("#select-model-all").change(function () {
     $(".checkbox-model").prop('checked', $(this).prop("checked"));
 });
