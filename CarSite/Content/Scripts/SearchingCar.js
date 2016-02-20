68756fdeec773302a981bc58a6cea6ed04e8f160
thaiwhere <thaiwhere@gmail.com>
2016-02-20 12:24:58 +0700

    modify SearchingCar for width

---
+++ CarSite/Content/Scripts/SearchingCar.js
@@ -0,0 +1,4 @@
+﻿$("#div-left").hide();
+$("#search-car-firm").show();
+$("#span_searching").removeClass("expand_searching").addClass("collapse_searching");
+$("#div-center").removeClass("div-center").addClass("div-center-searching");
