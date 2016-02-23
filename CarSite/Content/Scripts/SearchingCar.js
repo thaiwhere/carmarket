81dba1c2d32a79ad1743e5043972fd89196dd5c0
thai.tran <thai.tran@nexcel.vn>
2016-02-23 17:06:50 +0700

    Add new icon for searching car

--- CarSite/Content/Scripts/SearchingCar.js
+++ CarSite/Content/Scripts/SearchingCar.js
@@ -3,16 +3,20 @@ function generateData(numberRows, autoHeight) {
     var rows = [];
     for (var index = 0; index < numberRows; index++) {
 
+        var statusIcon = index % 2 == 0 ? "<div class='status-icon'></div>" : "";
+        var source = "<div class='car-info-source'>Nhập khẩu</div>";
         var href = "http://banxehoi.com/xe-daewoo-lacetti-ha-noi/ban-xe--dang-ky-2009-tu-nhan-mau-den-con-moi-318-trieu-aid630947";
+
         var title = "Bán Xe Daewoo Lacetti đăng ký 2009 tư nhân, màu đen còn mới, 318 triệu... (28/02/2016) ";
         var image = "/content/images/cars/20151227214928-b769_wm.jpg";
         var content = "Tôi cần bán xe Daewoo Laceti sản xuất năm 2009 tên tư nhân xe gia đình sử dụng nên còn chất lượng máy móc êm nội thất đẹp lốp mới đăng kiểm còn dài giá tốt 318 triệu liên hệ Mr Hải 0915558358.";
-        var car = "<div class='car-photo'><a href='" + href + "'><img title='" + title + "' src='" + image + "' alt='" + title + "'></a></div>";
-        var description = "<div class='car-type1'><a href='" + href + "'>" + title + "</a><br />" + content +"</div>";
         
-        var param = "<div class='car-info-item'>Tình trạng:&nbsp;Cũ</div><div class='car-info-item'>Xuất xứ:&nbsp;Nhập khẩu</div><div class='car-info-item'>Km:&nbsp;40.000 km</div><div class='car-info-item'>Hộp số:&nbsp;Số tự động</div>";
+        var param = "<div class='car-info-item'>Km:&nbsp;40.000 km</div><div class='car-info-item'>Hộp số:&nbsp;Số tự động</div>";
         var price_location = "<div class='car-info-price'>350 triệu</div><div class='car-info-place'><a href='/xe-toyota-vios-ha-noi'>Hà Nội</a></div>";
         var contact = "<div class='car-info-user'>Chị Oanh</div><div class='car-info-tel'>0901500799</div>";
+
+        var car = statusIcon + source + "<div class='car-photo'>" + "<a href='" + href + "'><img title='" + title + "' src='" + image + "' alt='" + title + "'></a></div>";
+        var description = "<div class='car-des-title'><a href='" + href + "'>" + title + "</a><br /><div class='car-des'>" + content + "</div></div>";
         var info = "<div class='car-info'>" + param + price_location + contact + "</div>";
 
         var row = {
@@ -31,12 +35,12 @@ function generateData(numberRows, autoHeight) {
 var gridOptions =
     {
         columns: [
-            { HeaderText: "Car", Width: 150, Name: "Car", HeaderAlign: "center", CellAlign: "center"},
-            { HeaderText: "Description", Width: 420, Name: "Description", HeaderAlign: "center", CellAlign: "center" },
-            { HeaderText: "Info", Width: 140, Name: "Info", HeaderAlign: "center", CellAlign: "center" }            
+            { HeaderText: "Hình", Width: 150, Name: "Car", HeaderAlign: "center", CellAlign: "center" },
+            { HeaderText: "Mô tả", Width: 420, Name: "Description", HeaderAlign: "center", CellAlign: "center" },
+            { HeaderText: "Thông tin", Width: 140, Name: "Info", HeaderAlign: "center", CellAlign: "center" }
         ],
         bodyRows: generateData(20),
-        gridExpandHeight: 100,
+        gridExpandHeight: 150,
         showPager: true,
         customPager: true,
         pagerOption: {
