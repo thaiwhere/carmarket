eb3663cad90db8821e0d615069ebb4de5b1dbed9
thai.tran <thai.tran@nexcel.vn>
2016-04-04 14:42:51 +0700

    #update grid

--- CarSite/Content/Scripts/base/GridRender.js
+++ CarSite/Content/Scripts/base/GridRender.js
@@ -44,27 +44,35 @@ var gridRender = function () {
         var list = [];
 
         for (var i = 0; i < cars.length; i++) {
-            var statusIcon = cars[i].Status == true ? "<div class='status-icon'></div>" : "";
-            var source = "<div class='car-info-source'>" + cars[i].Source + "</div>";
+            var href = cars[i].Href + "/1.jpg";// "/Car/CarDetail";                
+            var title = cars[i].Title;// "Bán Xe Daewoo Lacetti đăng ký 2009 tư nhân, màu đen còn mới, 318 triệu... (28/02/2016) ";            
 
-            var href = cars[i].Href;// "/Car/CarDetail";                
-            var title = cars[i].Title;// "Bán Xe Daewoo Lacetti đăng ký 2009 tư nhân, màu đen còn mới, 318 triệu... (28/02/2016) ";
-            var image = href + "/1.jpg"; //cars[i].Image;// "/content/images/cars/0" + (index + 1) + ".jpg";
+            var year = " (2015)";
+            var status = cars[i].Status == true ? "<div class='car-info-status-new'>" + "Xe mới" : "<div class='car-info-status-old'>" + "Xe cũ";
+            status += year + "</div>";
 
+            var source = "<div class='car-info-source'>" + (cars[i].Source == true ? "Nhập khẩu" : "Trong nước") + "</div>";
+            var photo = "<div class='car-photo'>" + "<a href='" + href + "'><img title='" + title + "' src='" + href + "' alt='" + title + "'></a></div>";
+            var firm = "<div class='car-info-firm'>" + "Toyota" + "</div>";
+
+            var car = status + source + photo + firm;
+            
             var content = cars[i].Content// "Tôi cần bán xe Daewoo Laceti sản xuất năm 2009 tên tư nhân xe gia đình sử dụng nên còn chất lượng máy móc êm nội thất đẹp lốp mới đăng kiểm còn dài giá tốt 318 triệu liên hệ Mr Hải 0915558358.";
 
             var param = "<div class='car-info-item'>Km:&nbsp;" + cars[i].Km + "km</div><div class='car-info-item'>Hộp số:&nbsp;" + cars[i].Type + "</div>";
             var price_location = "<div class='car-info-price'>" + cars[i].Price + " triệu</div><div class='car-info-place'><a href='" + href + "'>" + cars[i].Location + "</a></div>";
             var contact = "<div class='car-info-user'>" + cars[i].ContactName + "</div><div class='car-info-tel'>" + cars[i].ContactTel + "</div>";
 
-            var car = statusIcon + source + "<div class='car-photo'>" + "<a href='" + href + "'><img title='" + title + "' src='" + image + "' alt='" + title + "'></a></div>";
-            var description = "<div class='car-des-title'><a href='" + href + "'>" + title + "</a><br /><div class='car-des'>" + content + "</div></div>";
+            
+
+            var title = "<div class='car-des-title'><a href='" + href + "'>" + title + "</a></div>";
+            var description = "<div class='car-des'>" + content + "</div>";
             var info = "<div class='car-info'>" + param + price_location + contact + "</div>";
 
             var row = {
                 Columns: [
                         { Name: "Car", Value: car },
-                        { Name: "Description", Value: description },
+                        { Name: "Description", Value: title + description },
                         { Name: "Info", Value: info }
                 ]
             };
