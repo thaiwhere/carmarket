b47080aa95c92d7032529df5719b4c1c3dbcc09f
thai.tran <thai.tran@nexcel.vn>
2017-02-13 13:49:59 +0700

    #config route for news

--- CarSite/App_Start/RouteConfig.cs
+++ CarSite/App_Start/RouteConfig.cs
@@ -14,10 +14,18 @@ namespace CarSite
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+              name: "News",
+              url: "news/{id}",
+              defaults: new { controller = "News", action = "Index", id = UrlParameter.Optional }
+          );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+           
         }
     }
 }
